de154aa850ce7ac81891912307ebeedbee5ad0b3
rofikkerneldev <rofikoga@gmail.com>
2026-05-25 04:06:48 +0200

    uname -a

--- kernel/sys.c
+++ kernel/sys.c
@@ -1242,9 +1242,17 @@ static int override_release(char __user *release, size_t len)
 
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
-	struct new_utsname tmp;
+  struct new_utsname tmp;
 
-	down_read(&uts_sem);
+  down_read(&uts_sem);
+  memcpy(&tmp, utsname(), sizeof(tmp));
+  if (!strncmp(current->comm, "bpfloader", 9) ||
+      !strncmp(current->comm, "netbpfload", 10) ||
+      !strncmp(current->comm, "netd", 4)) {
+    strcpy(tmp.release, "5.10.239");
+    pr_debug("fake uname: %s/%d release=%s\n",
+       current->comm, current->pid, tmp.release);
+  }
 	memcpy(&tmp, utsname(), sizeof(tmp));
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
